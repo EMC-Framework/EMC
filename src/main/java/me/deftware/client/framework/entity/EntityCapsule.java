e72b20eb25f8962b86128021c14882d8b328d711
deftware <2382734-deftware@users.noreply.gitlab.com>
2020-12-22 13:50:28 +0100

    Add EntityRegistry

---
+++ src/main/java/me/deftware/client/framework/entity/EntityCapsule.java
@@ -0,0 +1,62 @@
+package me.deftware.client.framework.entity;
+
+import me.deftware.client.framework.chat.ChatMessage;
+import me.deftware.client.framework.util.minecraft.MinecraftIdentifier;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.IRegistry;
+
+import java.util.Objects;
+
+/**
+ * @author Deftware
+ */
+public class EntityCapsule {
+
+    private final EntityType<? extends Entity> entityType;
+    private ResourceLocation texture;
+    private final String id;
+
+    public <T extends Entity> EntityCapsule(String id, EntityType<T> entityType) {
+        this.entityType = entityType;
+        this.id = id;
+    }
+
+    public me.deftware.client.framework.entity.Entity create() {
+        return me.deftware.client.framework.entity.Entity.newInstance(entityType.create(
+                Objects.requireNonNull(Minecraft.getInstance().world)
+        ));
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public EntityType<? extends Entity> getRaw() {
+        return entityType;
+    }
+
+    public ChatMessage getName() {
+        return new ChatMessage().fromText(entityType.getName());
+    }
+
+    public void setTexture(ResourceLocation identifier) {
+        texture = identifier;
+    }
+
+    public MinecraftIdentifier getTexture() {
+        if (texture == null) return null;
+        return new MinecraftIdentifier(texture);
+    }
+
+    public MinecraftIdentifier getIdentifier() {
+        return new MinecraftIdentifier(Objects.requireNonNull(IRegistry.ENTITY_TYPE.getKey(entityType)));
+    }
+
+    public String getTranslationKey() {
+        return entityType.getTranslationKey();
+    }
+
+}
