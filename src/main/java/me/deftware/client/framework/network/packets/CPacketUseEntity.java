204756f671c21829a5d2a99c2053066514db0812
deftware <2382734-deftware@users.noreply.gitlab.com>
2021-01-12 19:00:06 +0100

    Sync

--- src/main/java/me/deftware/client/framework/network/packets/CPacketUseEntity.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketUseEntity.java
@@ -2,14 +2,35 @@ package me.deftware.client.framework.network.packets;
 
 import me.deftware.client.framework.entity.Entity;
 import me.deftware.client.framework.network.PacketWrapper;
+import net.minecraft.network.Packet;
 
 /**
  * @author Deftware
  */
 public class CPacketUseEntity extends PacketWrapper {
 
+    public CPacketUseEntity(Packet<?> packet) {
+        super(packet);
+    }
+
     public CPacketUseEntity(Entity entity) {
         super(new net.minecraft.network.play.client.CPacketUseEntity(entity.getMinecraftEntity()));
+    }
+
+    public Type getType() {
+        switch (((net.minecraft.network.play.client.CPacketUseEntity) packet).getAction()) {
+            case ATTACK:
+                return Type.ATTACK;
+            case INTERACT:
+                return Type.INTERACT;
+            case INTERACT_AT:
+                return Type.INTERACT_AT;
+        }
+        return Type.UNKNOWN;
+    }
+
+    public enum Type {
+        ATTACK, INTERACT, INTERACT_AT, UNKNOWN
     }
 
 }
