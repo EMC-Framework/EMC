f24d07f4e931efa039786bead8dd21a72fa0935c
deftware <deftware@aristois.net>
2021-03-12 14:13:25 +0100

    Add PacketRegistry

--- src/main/java/me/deftware/client/framework/network/packets/CPacketPosition.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketPosition.java
@@ -2,19 +2,18 @@ package me.deftware.client.framework.network.packets;
 
 import me.deftware.client.framework.network.PacketWrapper;
 import net.minecraft.network.Packet;
-import net.minecraft.network.play.client.CPacketPlayer;
 
 /**
  * @author Deftware
  */
-public class CPacketPosition extends PacketWrapper {
+public class CPacketPosition extends CPacketPlayer {
 
     public CPacketPosition(Packet<?> packet) {
         super(packet);
     }
 
     public CPacketPosition(double xIn, double yIn, double zIn, boolean onGroundIn) {
-        super(new CPacketPlayer.Position(xIn, yIn, zIn, onGroundIn));
+        super(new net.minecraft.network.play.client.CPacketPlayer.Position(xIn, yIn, zIn, onGroundIn));
     }
 
 }
--- src/main/java/me/deftware/client/framework/network/packets/CPacketPositionRotation.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketPositionRotation.java
@@ -1,20 +1,18 @@
 package me.deftware.client.framework.network.packets;
 
-import me.deftware.client.framework.network.PacketWrapper;
 import net.minecraft.network.Packet;
-import net.minecraft.network.play.client.CPacketPlayer;
 
 /**
  * @author Deftware
  */
-public class CPacketPositionRotation extends PacketWrapper {
+public class CPacketPositionRotation extends CPacketPlayer {
 
 	public CPacketPositionRotation(Packet<?> packet) {
 		super(packet);
 	}
 
 	public CPacketPositionRotation(double x, double y, double z, float yaw, float pitch, boolean isOnGround) {
-		super(new CPacketPlayer.PositionRotation(x,y,z,yaw,pitch,isOnGround));
+		super(new net.minecraft.network.play.client.CPacketPlayer.PositionRotation(x,y,z,yaw,pitch,isOnGround));
 	}
 
 }
