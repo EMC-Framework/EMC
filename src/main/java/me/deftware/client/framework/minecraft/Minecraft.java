b35deb3f4a9f151dfd9e3c800b3759c698bc6880
wagyourtail <wagyourtail@wagyourtail.xyz>
2021-04-12 15:23:37 -0700

    add isOnRealms()

--- src/main/java/me/deftware/client/framework/minecraft/Minecraft.java
+++ src/main/java/me/deftware/client/framework/minecraft/Minecraft.java
@@ -174,7 +174,11 @@ public class Minecraft {
 	public static ServerConnectionInfo getConnectedServer() {
 		return connectedServer.get();
 	}
-
+	
+	public static boolean isOnRealms() {
+		return net.minecraft.client.Minecraft.getMinecraft().isConnectedToRealms();
+	}
+	
 	@Nullable
 	public static ServerConnectionInfo getLastConnectedServer() {
 		return lastConnectedServer;
