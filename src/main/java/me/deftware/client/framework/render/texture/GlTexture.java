e57ee29e68208ca4da4a93424f265312f91c0907
deftware <deftware@aristois.net>
2021-04-19 20:10:53 +0200

    Update textures

--- src/main/java/me/deftware/client/framework/render/texture/GlTexture.java
+++ src/main/java/me/deftware/client/framework/render/texture/GlTexture.java
@@ -1,10 +1,13 @@
 package me.deftware.client.framework.render.texture;
 
+import me.deftware.client.framework.gui.GuiScreen;
 import me.deftware.client.framework.main.EMCMod;
 import me.deftware.client.framework.util.ResourceUtils;
-import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL13;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -20,7 +23,7 @@ public class GlTexture {
 
     private int glId;
 
-    private final int textureWidth, textureHeight;
+    private final int textureWidth, textureHeight, scaling;
 
     public GlTexture(EMCMod mod, String asset) throws IOException {
         this(
@@ -37,9 +40,17 @@ public class GlTexture {
     }
 
     public GlTexture(BufferedImage image) {
+        this(image, GL11.GL_LINEAR);
+    }
+
+    public GlTexture(BufferedImage image, int scaling) {
+        this.scaling = scaling;
         this.textureWidth = image.getWidth();
         this.textureHeight = image.getHeight();
-        glId = GraphicsUtil.loadTextureFromBufferedImage(image);
+        this.glId = GL11.glGenTextures();
+        GL13.glActiveTexture(GL13.GL_TEXTURE0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, this.glId);
+        this.upload(getImageBuffer(image), false);
     }
 
     public void draw(int x, int y, int width, int height) {
@@ -57,19 +68,30 @@ public class GlTexture {
     }
 
     public void upload(BufferedImage image) {
-        upload(
-                GraphicsUtil.getImageBuffer(image)
-        );
+        upload(getImageBuffer(image), true);
     }
 
-    public void upload(ByteBuffer buffer) {
+    /**
+     * If the texture is to be replaced, the size MUST match the previous size
+     */
+    public void upload(ByteBuffer buffer, boolean replace) {
         // Minecraft modifies these
         GL11.glPixelStorei(GL11.GL_UNPACK_ROW_LENGTH, 0);
         GL11.glPixelStorei(GL11.GL_UNPACK_SKIP_PIXELS, 0);
         GL11.glPixelStorei(GL11.GL_UNPACK_SKIP_ROWS, 0);
         GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 4);
+        // Clamping mode
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+        // Scaling
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, scaling);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, scaling);
         // Upload
-        GL11.glTexSubImage2D(GL11.GL_TEXTURE_2D, 0, 0, 0, textureWidth, textureHeight, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+        if (replace)
+            // Replace texture, without reallocating
+            GL11.glTexSubImage2D(GL11.GL_TEXTURE_2D, 0, 0, 0, textureWidth, textureHeight, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+        else
+            GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, textureWidth, textureHeight, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
     }
 
     public void destroy() {
@@ -82,12 +104,34 @@ public class GlTexture {
         return glId;
     }
 
+    public int getScaling() {
+        return scaling;
+    }
+
     public int getTextureWidth() {
         return textureWidth;
     }
 
     public int getTextureHeight() {
         return textureHeight;
+    }
+
+    public static ByteBuffer getImageBuffer(BufferedImage image) {
+        int width = image.getWidth(), height = image.getHeight();
+        int[] pixels = new int[width * height];
+        image.getRGB(0, 0, width, height, pixels, 0, image.getWidth());
+        ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * 4); // 4 for RGBA, 3 for RGB
+        for (int y = 0; y < image.getHeight(); y++) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                int pixel = pixels[y * image.getWidth() + x];
+                buffer.put((byte) ((pixel >> 16) & 0xFF));    // Red component
+                buffer.put((byte) ((pixel >> 8) & 0xFF));     // Green component
+                buffer.put((byte) (pixel & 0xFF));            // Blue component
+                buffer.put((byte) ((pixel >> 24) & 0xFF));    // Alpha component. Only for RGBA
+            }
+        }
+        buffer.flip();
+        return buffer;
     }
 
 }
