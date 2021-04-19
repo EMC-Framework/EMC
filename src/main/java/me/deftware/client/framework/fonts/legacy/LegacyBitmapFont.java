623cd9b96d650ae8eae8b0131bc3095bf6666b60
deftware <deftware@aristois.net>
2021-04-19 20:10:53 +0200

    Update textures

--- src/main/java/me/deftware/client/framework/fonts/legacy/LegacyBitmapFont.java
+++ src/main/java/me/deftware/client/framework/fonts/legacy/LegacyBitmapFont.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import me.deftware.client.framework.main.bootstrap.Bootstrap;
 import me.deftware.client.framework.registry.font.TTFRegistry;
 import me.deftware.client.framework.render.batching.RenderStack;
+import me.deftware.client.framework.render.texture.GlTexture;
 import me.deftware.client.framework.render.texture.GraphicsUtil;
 import me.deftware.client.framework.util.path.OSUtils;
 import org.lwjgl.opengl.GL11;
@@ -43,7 +44,7 @@ public class LegacyBitmapFont {
     public int shadow = 1;
 
     @Getter
-    public int glId = -1;
+    private GlTexture textureAtlas;
 
     @Getter
     public int textureWidth, textureHeight;
@@ -104,10 +105,10 @@ public class LegacyBitmapFont {
             characters.add(specialCharacter);
         }
 
-        glId = characterGenerate(characters);
+        textureAtlas = characterGenerate(characters);
     }
 
-    protected int characterGenerate(List<Character> characters) {
+    protected GlTexture characterGenerate(List<Character> characters) {
         // Calculate size of texture
         // fixedWidth must be more than the width of the widest character
         int width = 0, height = 0, fixedWidth = getStringWidth('W') * 2;
@@ -151,10 +152,9 @@ public class LegacyBitmapFont {
         long sizeBytes = ((long) dataBuffer.getSize()) * 4L;
         long sizeMB = sizeBytes / (1024L * 1024L);
 
-        Bootstrap.logger.info("Font atlas {}x{}, {} megabytes", textureWidth, textureHeight, sizeMB);
+        Bootstrap.logger.debug("Font atlas {}x{}, {} megabytes", textureWidth, textureHeight, sizeMB);
 
-        // Upload to gpu
-        return GraphicsUtil.loadTextureFromBufferedImage(characterTexture);
+        return new GlTexture(characterTexture);
     }
 
     public int getStringWidth(char... chars) {
@@ -170,11 +170,10 @@ public class LegacyBitmapFont {
     }
 
     public void destroy() {
-        if (glId != -1) {
-            GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
-            GL11.glDeleteTextures(glId);
+        if (textureAtlas != null) {
+            textureAtlas.destroy();
             characterMap.clear();
-            glId = -1;
+            textureAtlas = null;
         }
     }
 
