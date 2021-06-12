8f061d8710d8f405c593ef1952b7e63903cfbd67
deftware <deftware@aristois.net>
2021-06-12 17:21:13 +0200

    Update shader system

---
+++ src/main/java/me/deftware/client/framework/resource/ModResource.java
@@ -0,0 +1,55 @@
+package me.deftware.client.framework.resource;
+
+import net.minecraft.client.resources.IResource;
+import net.minecraft.client.resources.data.IMetadataSection;
+import net.minecraft.util.ResourceLocation;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Deftware
+ */
+public class ModResource implements IResource {
+
+    private final InputStream stream;
+    private final ResourceLocation id;
+
+    public ModResource(InputStream stream, ResourceLocation id) {
+        this.id = id;
+        this.stream = stream;
+    }
+
+    @Override
+    public ResourceLocation getResourceLocation() {
+        return id;
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return stream;
+    }
+
+    @Override
+    public boolean hasMetadata() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public <T extends IMetadataSection> T getMetadata(String s) {
+        return null;
+    }
+
+    @Override
+    public String getResourcePackName() {
+        return "Minecraft";
+    }
+
+    @Override
+    public void close() throws IOException {
+        stream.close();
+    }
+
+}
---
+++ src/main/java/me/deftware/client/framework/resource/ModResourceManager.java
@@ -0,0 +1,57 @@
+package me.deftware.client.framework.resource;
+
+import com.google.common.collect.Sets;
+import me.deftware.client.framework.main.EMCMod;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.IResource;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+/**
+ * @author Deftware
+ */
+public class ModResourceManager implements IResourceManager {
+
+    private final Set<String> namespaces = Sets.newLinkedHashSet();
+    private final ZipFile zipFile;
+    private final String type;
+
+    public ModResourceManager(EMCMod mod, String type) throws IOException {
+        this.zipFile = getZipFile(mod);
+        this.namespaces.add(mod.getMeta().getName().toLowerCase());
+        this.type = type;
+    }
+
+    public ZipFile getZipFile(EMCMod mod) throws IOException {
+        return new ZipFile(mod.physicalFile);
+    }
+
+    public ZipEntry getEntry(String name) {
+        return zipFile.getEntry(type + "/" + name);
+    }
+
+    @Override
+    public List<IResource> getAllResources(ResourceLocation id) throws IOException {
+        return Collections.singletonList(getResource(id));
+    }
+
+    @Override
+    public Set<String> getResourceDomains() {
+        return namespaces;
+    }
+
+    @Override
+    public IResource getResource(ResourceLocation id) throws IOException {
+        ZipEntry entry = getEntry(id.getPath());
+        if (entry == null) {
+            return Minecraft.getMinecraft().getResourceManager().getResource(id);
+        }
+        return new ModResource(zipFile.getInputStream(entry), id);
+    }
+
+}
