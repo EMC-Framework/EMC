7606cc53d90ada672001c53337e1e690b9f91cfd
deftware <deftware@aristois.net>
2021-06-16 17:49:08 +0200

    Always render buffer

--- src/main/java/me/deftware/mixin/mixins/shader/MixinWorldRenderer.java
+++ src/main/java/me/deftware/mixin/mixins/shader/MixinWorldRenderer.java
@@ -90,10 +90,8 @@ public abstract class MixinWorldRenderer {
             GlStateManager.enableBlend();
             GlStateManager.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE);
             for (Shader shader : Shader.SHADERS) {
-                if (shader.isRender()) {
                     shader.getFramebuffer().framebufferRenderExt(this.mc.mainWindow.getFramebufferWidth(), this.mc.mainWindow.getFramebufferHeight(), false);
                     shader.setRender(false);
-                }
             }
             GlStateManager.disableBlend();
             mc.getFramebuffer().bindFramebuffer(false);
