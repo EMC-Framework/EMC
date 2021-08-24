dd6a769a2a5d93c4fe3fe511f0de69260a42f0e4
deftware <deftware@aristois.net>
2021-08-24 14:17:12 +0200

    Fix post draw event in containers

--- src/main/java/me/deftware/mixin/mixins/gui/MixinGuiContainer.java
+++ src/main/java/me/deftware/mixin/mixins/gui/MixinGuiContainer.java
@@ -11,6 +11,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerScreen.class)
 public abstract class MixinGuiContainer<T extends Container> extends MixinGuiScreen implements me.deftware.client.framework.gui.screens.ContainerScreen {
@@ -52,6 +55,11 @@ public abstract class MixinGuiContainer<T extends Container> extends MixinGuiScr
     @Override
     public ChatMessage getInventoryName() {
         return inventoryTitle.get();
+    }
+
+    @Inject(method = "render", at = @At("RETURN"))
+    private void onPostDraw(int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        this.onPostDrawEvent(mouseX, mouseY, delta);
     }
 
 }
