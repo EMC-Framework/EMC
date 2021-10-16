c7e687c1f87a9449a386d0d6e3620ea24f730e73
deftware <deftware@aristois.net>
2021-10-16 15:00:41 +0200

    Fix crash

--- src/main/java/me/deftware/mixin/mixins/render/MixinParticleManager.java
+++ src/main/java/me/deftware/mixin/mixins/render/MixinParticleManager.java
@@ -4,19 +4,25 @@ import me.deftware.client.framework.event.events.EventParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.particle.ParticleEffect;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ParticleManager.class)
-public class MixinParticleManager {
+public abstract class MixinParticleManager {
+
+    @Shadow
+    @Nullable
+    protected abstract <T extends ParticleEffect> Particle createParticle(T parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ);
 
     @Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
     private void onAddParticle(ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> ci) {
         EventParticle event = new EventParticle(parameters.asString(), x, y, z, velocityX, velocityY, velocityZ).broadcast();
         if (event.isCanceled()) {
-            ci.cancel();
+            ci.setReturnValue(this.createParticle(parameters, x, y, z, velocityX, velocityY, velocityZ));
         }
     }
 
