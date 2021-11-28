0d43ec0b74607f43e081ff45e93f1456105d0ae8
deftware <deftware@aristois.net>
2021-11-28 15:51:01 +0100

    Add Chunk access

--- src/main/java/me/deftware/client/framework/world/block/Block.java
+++ src/main/java/me/deftware/client/framework/world/block/Block.java
@@ -11,10 +11,14 @@ import me.deftware.client.framework.world.block.types.CropBlock;
 import me.deftware.client.framework.world.block.types.ShulkerBlock;
 import me.deftware.client.framework.world.block.types.StorageBlock;
 import net.minecraft.block.*;
+import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.IRegistry;
 
-import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Deftware
@@ -62,8 +66,10 @@ public class Block implements IItem, SelectableList.ListItem, Identifiable {
 		return false;
 	}
 
-	public BlockState getDefaultBlockState() {
-		return new BlockState(block.getDefaultState());
+	public InputStream getAsset() throws IOException {
+		ResourceLocation blockResource = net.minecraft.block.Block.REGISTRY.getNameForObject(block);
+		ResourceLocation blockTexture = new ResourceLocation(blockResource.getNamespace(), "textures/blocks/" + blockResource.getPath() + ".png");
+		return Minecraft.getMinecraft().getResourceManager().getResource(blockTexture).getInputStream();
 	}
 
 	public boolean isAir() {
---
+++ src/main/java/me/deftware/mixin/mixins/world/MixinChunk.java
@@ -0,0 +1,31 @@
+package me.deftware.mixin.mixins.world;
+
+import me.deftware.client.framework.world.chunk.ChunkAccessor;
+import me.deftware.client.framework.world.chunk.SectionAccessor;
+import net.minecraft.world.chunk.Chunk;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Chunk.class)
+public class MixinChunk implements ChunkAccessor {
+
+    @Override
+    public SectionAccessor getSection(int index) {
+        return (SectionAccessor) ((Chunk) (Object) this).getBlockStorageArray()[index];
+    }
+
+    @Override
+    public int getChunkPosX() {
+        return ((Chunk) (Object) this).getPos().x;
+    }
+
+    @Override
+    public int getChunkPosZ() {
+        return ((Chunk) (Object) this).getPos().z;
+    }
+
+    @Override
+    public int getChunkHeight() {
+        return 256;
+    }
+
+}
