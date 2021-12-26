7f8ab9674ae6275cfbe91c1375bc8f4f09b80646
Wagyourtail <wagyourtail@wagyourtail.xyz>
2021-12-25 17:13:40 -0700

    dd some blockstate data, and world.hasChunk

--- src/main/java/me/deftware/mixin/mixins/world/MixinWorld.java
+++ src/main/java/me/deftware/mixin/mixins/world/MixinWorld.java
@@ -103,6 +103,11 @@ public abstract class MixinWorld implements me.deftware.client.framework.world.W
 	}
 
 	@Override
+	public boolean _hasChunk(int x, int z) {
+		return ((World) (Object) this).isChunkLoaded(x, z);
+	}
+
+	@Override
 	public int _getDimension() {
 		RegistryKey<World> key = ((World) (Object) this).getRegistryKey();
 		if (World.END.equals(key)) {
