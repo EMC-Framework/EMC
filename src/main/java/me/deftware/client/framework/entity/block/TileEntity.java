b514697cd77a392e6cabfa9edd7a172096941ae2
Wagyourtail <wagyourtail@wagyourtail.xyz>
2022-02-24 16:19:00 -0700

    fix missing getBlock in tileentity

--- src/main/java/me/deftware/client/framework/entity/block/TileEntity.java
+++ src/main/java/me/deftware/client/framework/entity/block/TileEntity.java
@@ -4,8 +4,13 @@ import me.deftware.client.framework.entity.Entity;
 import me.deftware.client.framework.math.box.BoundingBox;
 import me.deftware.client.framework.math.position.BlockPosition;
 import me.deftware.client.framework.math.position.TileBlockPosition;
+import me.deftware.client.framework.registry.BlockRegistry;
+import me.deftware.client.framework.world.block.Block;
 import net.minecraft.tileentity.TileEntityEnderChest;
 import net.minecraft.tileentity.TileEntityLockableLoot;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Optional;
 
 /**
  * @author Deftware
@@ -15,6 +20,8 @@ public class TileEntity {
 	private BoundingBox SINGLE;
 	protected final net.minecraft.tileentity.TileEntity entity;
 	protected final BlockPosition position;
+
+	protected Block block;
 
 	public static TileEntity newInstance(net.minecraft.tileentity.TileEntity entity) {
 		if (entity instanceof TileEntityLockableLoot || entity instanceof TileEntityEnderChest) {
@@ -37,10 +44,21 @@ public class TileEntity {
 	protected TileEntity(net.minecraft.tileentity.TileEntity entity) {
 		this.entity = entity;
 		this.position = new TileBlockPosition(entity);
+		ResourceLocation identifier = net.minecraft.tileentity.TileEntity.getKey(entity.getClass());
+		if (identifier != null) {
+			Optional<Block> block = BlockRegistry.INSTANCE.find(
+				identifier.getPath()
+			);
+			block.ifPresent(value -> this.block = value);
+		}
 	}
 
 	public String getClassName() {
 		return this.getClass().getSimpleName().substring(0, this.getClass().getSimpleName().length() - "Entity".length());
+	}
+
+	public Block getBlock() {
+		return block;
 	}
 
 	public BlockPosition getBlockPosition() {
