37bd77773e5eddc97db31004e045271c75331089
deftware <deftware@aristois.net>
2022-06-04 16:45:26 +0200

    Ignore firework particles in EventParticle

--- src/main/java/me/deftware/mixin/mixins/render/MixinParticleManager.java
+++ src/main/java/me/deftware/mixin/mixins/render/MixinParticleManager.java
@@ -4,19 +4,28 @@ import me.deftware.client.framework.event.events.EventParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(ParticleManager.class)
 public class MixinParticleManager {
 
+    @Unique
+    private final List<ParticleEffect> IGNORED_PARTICLES = List.of(ParticleTypes.FIREWORK, ParticleTypes.FLASH);
+
     @Inject(method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;", at = @At("HEAD"), cancellable = true)
     private void onAddParticle(ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, CallbackInfoReturnable<Particle> ci) {
-        EventParticle event = new EventParticle(parameters.asString(), x, y, z, velocityX, velocityY, velocityZ).broadcast();
-        if (event.isCanceled()) {
-            ci.cancel();
+        if (!IGNORED_PARTICLES.contains(parameters)) {
+            EventParticle event = new EventParticle(parameters.asString(), x, y, z, velocityX, velocityY, velocityZ).broadcast();
+            if (event.isCanceled()) {
+                ci.cancel();
+            }
         }
     }
 
