8179084e32f0a488f73a3a9fc1b2858dfdba8175
deftware <deftware@aristois.net>
2022-06-11 15:16:55 +0200

    Add better exception handling to the EventBus

--- src/main/java/me/deftware/client/framework/event/Listener.java
+++ src/main/java/me/deftware/client/framework/event/Listener.java
@@ -1,6 +1,7 @@
 package me.deftware.client.framework.event;
 
 import java.lang.reflect.Method;
+import java.util.function.Consumer;
 
 /**
  * @author Deftware
@@ -10,6 +11,8 @@ public class Listener {
     private final Method method;
     private final Object classInstance;
     private final int priority;
+
+    private Consumer<Throwable> exceptionHandler;
 
     public Listener(Method method, Object classInstance, int priority) {
         this.method = method;
@@ -27,6 +30,18 @@ public class Listener {
 
     public void invoke(Event event) throws Exception {
         method.invoke(getClassInstance(), event);
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public Consumer<Throwable> getExceptionHandler() {
+        return exceptionHandler;
+    }
+
+    public void setExceptionHandler(Consumer<Throwable> exceptionHandler) {
+        this.exceptionHandler = exceptionHandler;
     }
 
 }
