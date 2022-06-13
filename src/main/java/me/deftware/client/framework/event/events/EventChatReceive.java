b9a857658009f1551f822ce1d02ed4c48111a8ca
deftware <deftware@aristois.net>
2022-06-13 17:03:56 +0200

    Add sender modification to EventChatReceive

--- src/main/java/me/deftware/client/framework/event/events/EventChatReceive.java
+++ src/main/java/me/deftware/client/framework/event/events/EventChatReceive.java
@@ -1,9 +1,8 @@
 package me.deftware.client.framework.event.events;
 
-import lombok.Getter;
-import lombok.Setter;
 import me.deftware.client.framework.chat.ChatMessage;
 import me.deftware.client.framework.event.Event;
+import net.minecraft.network.message.MessageSender;
 import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 
 import java.time.Instant;
@@ -14,28 +13,48 @@ import java.util.UUID;
  */
 public class EventChatReceive extends Event {
 
-    @Setter
-    @Getter
     private ChatMessage message;
 
-    @Getter
     private final boolean signed, expired;
-
-    private final ChatMessageS2CPacket packet;
+    private MessageSender sender;
 
     public EventChatReceive(ChatMessageS2CPacket packet, boolean signed) {
         this.message = new ChatMessage().fromText(packet.signedContent());
         this.expired = packet.isExpired(Instant.now());
-        this.packet = packet;
         this.signed = signed;
+        this.sender = packet.sender();
     }
 
-    public ChatMessage getSender() {
-        return new ChatMessage().fromText(packet.sender().name());
+    public void setMessage(ChatMessage message) {
+        this.message = message;
     }
 
-    public UUID getSenderUUID() {
-        return packet.sender().uuid();
+    public ChatMessage getMessage() {
+        return message;
+    }
+
+    public boolean isSigned() {
+        return signed;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public MessageSender getSender() {
+        return this.sender;
+    }
+
+    public ChatMessage getSenderName() {
+        return new ChatMessage().fromText(sender.name());
+    }
+
+    public UUID getSenderId() {
+        return sender.uuid();
+    }
+
+    public void setSender(ChatMessage name) {
+        this.sender = new MessageSender(this.sender.uuid(), name.build());
     }
 
 }
