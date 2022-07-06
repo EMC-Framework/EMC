29d589d3495c5fede1b5d4ea636f0e41d1a2e3b0
deftware <deftware@aristois.net>
2022-07-05 20:51:06 -0700

    Update optifine block model renderer mixin
    Only rerun block render if cull is enabled
    
    Cancel method after calling runnable

--- src/main/java/me/deftware/mixin/mixins/integration/MixinOptiFineBlockModelRenderer.java
+++ src/main/java/me/deftware/mixin/mixins/integration/MixinOptiFineBlockModelRenderer.java
@@ -10,6 +10,7 @@ import net.minecraft.client.render.block.BlockModelRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.BlockRenderView;
 import net.minecraftforge.client.model.data.IModelData;
@@ -20,10 +21,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Random;
-import java.util.function.Supplier;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  *  Compatible with OptiFine G9
@@ -36,34 +34,33 @@ import java.util.function.Supplier;
 public abstract class MixinOptiFineBlockModelRenderer {
 
     @Shadow(remap = false)
-    public abstract boolean renderModelSmooth(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data);
+    public abstract void renderModelSmooth(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data);
 
     @Shadow(remap = false)
-    public abstract boolean renderModelFlat(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data);
+    public abstract void renderModelFlat(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data);
 
-    @Inject(method = "renderModelSmooth", at = @At("RETURN"), remap = false, cancellable = true)
-    public void renderModelSmoothInject(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrix, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data, CallbackInfoReturnable<Boolean> ci) {
+    @Inject(method = "renderModelSmooth", at = @At("HEAD"), remap = false, cancellable = true)
+    public void renderModelSmoothInject(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrix, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, IModelData data, CallbackInfo ci) {
         onBlockModelRender(state.getBlock(), cull, ci, () -> renderModelSmooth(world, model, state, pos, matrix, vertexConsumer, false, random, seed, overlay, data));
     }
 
     @Inject(method = "renderModelFlat", at = @At("HEAD"), remap = false, cancellable = true)
-    public void renderModelFlatInject(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long l, int i, IModelData data, CallbackInfoReturnable<Boolean> ci) {
+    public void renderModelFlatInject(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack buffer, VertexConsumer vertexConsumer, boolean cull, Random random, long l, int i, IModelData data, CallbackInfo ci) {
         onBlockModelRender(state.getBlock(), cull, ci, () -> renderModelFlat(world, model, state, pos, buffer, vertexConsumer, false, random, l, i, data));
     }
 
     @Unique
-    private void onBlockModelRender(Block block, boolean cull, CallbackInfoReturnable<Boolean> ci, Supplier<Boolean> supplier) {
+    private void onBlockModelRender(Block block, boolean cull, CallbackInfo ci, Runnable runnable) {
         FrameworkConstants.CAN_RENDER_SHADER = !Config.isShaders();
         try {
             BlockPropertyManager blockProperties = Bootstrap.blockProperties;
             if (blockProperties.isActive() && !blockProperties.isOpacityMode()) {
                 int id = Registry.BLOCK.getRawId(block);
                 if (!(blockProperties.contains(id) && blockProperties.get(id).isRender())) {
-                    // Do not render block
-                    ci.setReturnValue(false);
+                    ci.cancel();
                 } else if (cull) {
-                    // Render block with culling disabled
-                    ci.setReturnValue(supplier.get());
+                    runnable.run();
+                    ci.cancel();
                 }
             }
         } catch (Exception ignored) { }
