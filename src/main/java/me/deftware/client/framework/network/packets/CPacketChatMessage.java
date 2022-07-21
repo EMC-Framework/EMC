67386fb2b8c3ec2f212764e9b5f3ee9e7f12ae28
deftware <deftware@aristois.net>
2022-07-21 11:28:27 -0700

    rc2

--- src/main/java/me/deftware/client/framework/network/packets/CPacketChatMessage.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketChatMessage.java
@@ -1,14 +1,15 @@
 package me.deftware.client.framework.network.packets;
 
+import com.mojang.brigadier.ParseResults;
 import me.deftware.client.framework.network.PacketWrapper;
+import net.minecraft.class_7644;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.command.CommandSource;
 import net.minecraft.network.Packet;
 import net.minecraft.network.encryption.Signer;
-import net.minecraft.network.message.DecoratedContents;
-import net.minecraft.network.message.LastSeenMessageList;
-import net.minecraft.network.message.MessageMetadata;
-import net.minecraft.network.message.MessageSignatureData;
+import net.minecraft.network.message.*;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
+import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +23,14 @@ public class CPacketChatMessage extends PacketWrapper {
     }
 
     public CPacketChatMessage(String text) {
-        super(message(text));
+        super(of(text));
+    }
+
+    public static Packet<?> of(String text) {
+        if (text.startsWith("/")) {
+            return command(text.substring(1));
+        }
+        return message(text);
     }
 
     public static ChatMessageC2SPacket message(String text) {
@@ -47,5 +55,39 @@ public class CPacketChatMessage extends PacketWrapper {
 
         return new ChatMessageC2SPacket(text, messageMeta.timestamp(), messageMeta.salt(), signature, true, acknowledgment);
     }
+
+    public static CommandExecutionC2SPacket command(String text) {
+        var client = MinecraftClient.getInstance();
+        var networkHandler = client.getNetworkHandler();
+        var preview = Text.of(text);
+        var player = client.player;
+
+        MessageMetadata messageMetadata = MessageMetadata.of(player.getUuid());
+        LastSeenMessageList.Acknowledgment acknowledgment = networkHandler.consumeAcknowledgment();
+
+        ParseResults<CommandSource> parseResults = networkHandler.getCommandDispatcher().parse(text, networkHandler.getCommandSource());
+        var argumentSignatureDataMap = signArguments(messageMetadata, acknowledgment.lastSeen(), parseResults, preview);
+
+        return new CommandExecutionC2SPacket(text, messageMetadata.timestamp(), messageMetadata.salt(), argumentSignatureDataMap, true, acknowledgment);
+    }
+
+    private static ArgumentSignatureDataMap signArguments(MessageMetadata messageMetadata, LastSeenMessageList lastSeen, ParseResults<CommandSource> parseResults, Text preview) {
+        var client = MinecraftClient.getInstance();
+        var networkHandler = client.getNetworkHandler();
+        Signer profileSigner = client.getProfileKeys().getSigner();
+        if (profileSigner == null) {
+            return ArgumentSignatureDataMap.EMPTY;
+        }
+        try {
+            return ArgumentSignatureDataMap.sign(class_7644.method_45043(parseResults), (argumentName, value) -> {
+                DecoratedContents decoratedContents = preview != null ? new DecoratedContents(value, preview) : new DecoratedContents(value);
+                return networkHandler.getMessagePacker().pack(profileSigner, messageMetadata, decoratedContents, lastSeen).signature();
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ArgumentSignatureDataMap.EMPTY;
+        }
+    }
+
 
 }
