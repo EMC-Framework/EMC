bcce6c7ca768622338c0492fa614df5efa75298f
deftware <deftware@aristois.net>
2022-11-22 17:58:05 +0100

    pre1

--- src/main/java/me/deftware/mixin/mixins/entity/MixinEntityType.java
+++ src/main/java/me/deftware/mixin/mixins/entity/MixinEntityType.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(EntityType.class)
 public class MixinEntityType {
 
-    @Redirect(method = "register", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/registry/Registry;register(Lnet/minecraft/util/registry/Registry;Ljava/lang/String;Ljava/lang/Object;)Ljava/lang/Object;", opcode = 182))
+    @Redirect(method = "register", at = @At(value = "INVOKE", target = "Lnet/minecraft/registry/Registry;register(Lnet/minecraft/registry/Registry;Ljava/lang/String;Ljava/lang/Object;)Ljava/lang/Object;", opcode = 182))
     private static <T> T registerRedirect(Registry<? super T> registry, String id, T entry) {
         T item = Registry.register(registry, id, entry);
         EntityType<?> type = (EntityType<?>) item;
--- src/main/java/me/deftware/mixin/mixins/network/MixinTelemetry.java
+++ src/main/java/me/deftware/mixin/mixins/network/MixinTelemetry.java
@@ -1,6 +1,6 @@
 package me.deftware.mixin.mixins.network;
 
-import net.minecraft.client.util.TelemetrySender;
+import net.minecraft.client.util.telemetry.TelemetryManager;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,10 +9,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 /**
  * Disables Minecraft telemetry
  */
-@Mixin(TelemetrySender.class)
+@Mixin(TelemetryManager.class)
 public class MixinTelemetry {
 
-	@Redirect(method = "<init>", at = @At(value = "FIELD", target = "Lnet/minecraft/SharedConstants;isDevelopment:Z", opcode = Opcodes.GETSTATIC))
+	@Redirect(method = "getSender", at = @At(value = "FIELD", target = "Lnet/minecraft/SharedConstants;isDevelopment:Z", opcode = Opcodes.GETSTATIC))
 	private boolean onIsDevelopment() {
 		return true;
 	}
