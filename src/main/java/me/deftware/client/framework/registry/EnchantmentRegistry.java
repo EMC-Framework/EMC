7486c90e3f65c8048910259870f526729f5a4bd3
deftware <deftware@aristois.net>
2023-04-15 20:33:21 +0200

    Reimplement enchantments and effects

--- src/main/java/me/deftware/client/framework/registry/EnchantmentRegistry.java
+++ src/main/java/me/deftware/client/framework/registry/EnchantmentRegistry.java
@@ -1,28 +1,22 @@
 package me.deftware.client.framework.registry;
 
-import me.deftware.client.framework.item.enchantment.Enchantment;
+import me.deftware.client.framework.item.Enchantment;
+import net.minecraft.util.registry.Registry;
 
-import java.util.HashMap;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
  * @author Deftware
  */
-public enum EnchantmentRegistry implements IRegistry.IdentifiableRegistry<Enchantment, net.minecraft.enchantment.Enchantment> {
+public enum EnchantmentRegistry implements IRegistry.IdentifiableRegistry<Enchantment, Void> {
 
 	INSTANCE;
 
-	private final HashMap<String, Enchantment> enchantments = new HashMap<>();
-
 	@Override
 	public Stream<Enchantment> stream() {
-		return enchantments.values().stream();
-	}
-
-	@Override
-	public void register(String id, net.minecraft.enchantment.Enchantment object) {
-		enchantments.putIfAbsent(id, new Enchantment(object));
+		return Registry.ENCHANTMENT
+				.stream()
+				.map(Enchantment.class::cast);
 	}
 
 }
