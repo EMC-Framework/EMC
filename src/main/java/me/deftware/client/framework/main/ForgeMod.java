84bd20177c4a1f5c33b6e210ea3806abde878ec0
deftware <deftware@aristois.net>
2023-06-23 11:57:02 +0200

    Fix stencil buffer

--- src/main/java/me/deftware/client/framework/main/ForgeMod.java
+++ src/main/java/me/deftware/client/framework/main/ForgeMod.java
@@ -1,6 +1,24 @@
 package me.deftware.client.framework.main;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod("emc")
-public class ForgeMod { }
+public class ForgeMod {
+
+    private final Logger logger = LoggerFactory.getLogger("EMC|Forge");
+
+    public ForgeMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onLoad);
+    }
+
+    private void onLoad(FMLClientSetupEvent event) {
+        logger.info("Configuring Forge for EMC");
+        MinecraftClient.getInstance().getFramebuffer().enableStencil();
+    }
+
+}
