7946b2934133205d065d2ed96b810fece7c3bfab
deftware <deftware@aristois.net>
2023-09-16 22:07:25 +0200

    Properly handle key events

--- src/main/java/me/deftware/mixin/mixins/input/MixinKeyboardListener.java
+++ src/main/java/me/deftware/mixin/mixins/input/MixinKeyboardListener.java
@@ -5,6 +5,7 @@ import me.deftware.client.framework.event.events.EventKeyAction;
 import me.deftware.client.framework.event.events.EventKeyActionRaw;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,14 +16,14 @@ public class MixinKeyboardListener {
 
     @Inject(method = "onKey", at = @At("HEAD"))
     private void onKeyEventRaw(long windowPointer, int keyCode, int scanCode, int action, int modifiers, CallbackInfo ci) {
-        if (windowPointer == MinecraftClient.getInstance().getWindow().getHandle())
+        var minecraft = MinecraftClient.getInstance();
+        if (windowPointer == minecraft.getWindow().getHandle()) {
             new EventKeyActionRaw(keyCode, action, modifiers).broadcast();
-    }
-
-    @Inject(method = "onKey", at = @At(value = "INVOKE", target = "net/minecraft/client/util/InputUtil.isKeyPressed(JI)Z", ordinal = 2))
-    private void onKeyEvent(long windowPointer, int keyCode, int scanCode, int action, int modifiers, CallbackInfo ci) {
-        if (keyCode >= 32 && keyCode <= 348) {
-            new EventKeyAction(keyCode, action, modifiers).broadcast();
+            if (minecraft.currentScreen == null && keyCode >= 32 && keyCode <= 348) {
+                if (action != GLFW.GLFW_RELEASE) {
+                    new EventKeyAction(keyCode, action, modifiers).broadcast();
+                }
+            }
         }
     }
 
