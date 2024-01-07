c0e045f851a818361366669a049ffa1705712204
deftware <deftware@aristois.net>
2024-01-07 14:07:26 +0100

    Allow raw access to offset in FontRenderStack

--- src/main/java/me/deftware/client/framework/render/batching/font/FontRenderStack.java
+++ src/main/java/me/deftware/client/framework/render/batching/font/FontRenderStack.java
@@ -97,7 +97,15 @@ public class FontRenderStack extends RenderStack<FontRenderStack> {
 		offset = 0;
 	}
 
-	public void renderCharBuffer(String[] buffer, int x, int y, int color) {
+	public AtlasTextureFont getFont() {
+		return font;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public int renderCharBuffer(String[] buffer, int x, int y, int color) {
 		// Scale position
 		if (scaled) {
 			x *= RenderStack.getScale();
@@ -156,6 +164,7 @@ public class FontRenderStack extends RenderStack<FontRenderStack> {
 			drawCharacter(x + offset, y, data);
 			offset += data.getWidth();
 		}
+		return offset;
 	}
 
 	public int getFontHeight() {
