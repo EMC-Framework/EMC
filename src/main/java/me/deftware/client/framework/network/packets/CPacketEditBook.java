a6c79abe4a914c75cc861a1b8dff74c378e0ba88
deftware <deftware@aristois.net>
2024-01-13 16:09:08 +0100

    Fix CPacketEditBook

--- src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
@@ -2,8 +2,13 @@ package me.deftware.client.framework.network.packets;
 
 import me.deftware.client.framework.item.ItemStack;
 import me.deftware.client.framework.network.PacketWrapper;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Deftware
@@ -14,8 +19,24 @@ public class CPacketEditBook extends PacketWrapper {
         super(packet);
     }
 
+    public CPacketEditBook(int slot, List<String> pages, Optional<String> title) {
+        super(new BookUpdateC2SPacket(slot, pages, title));
+    }
+
+    private static BookUpdateC2SPacket of(ItemStack book) {
+        var nbt = book.getNbt().getList("pages", 9);
+        if (nbt instanceof NbtList list) {
+            List<String> pages = new ArrayList<>(list.size());
+            for (int i = 0; i < list.size(); i++) {
+                pages.add(list.getString(i));
+            }
+            return new BookUpdateC2SPacket(0, pages, Optional.empty());
+        }
+        throw new IllegalArgumentException("ItemStack must be a book with a valid NBT");
+    }
+
     public CPacketEditBook(ItemStack book) {
-        super(null); //new BookUpdateC2SPacket(book.getMinecraftItemStack(), true, 0)); // TODO: Verify last variable (Main hand)
+        super(of(book));
     }
 
 }
