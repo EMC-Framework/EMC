1f0cea88e919610987fdb640ea1416ef4c042ef2
deftware <deftware@aristois.net>
2024-03-02 20:45:56 +0100

    Add nbt helper functions

--- src/main/java/me/deftware/client/framework/item/items/BlockItem.java
+++ src/main/java/me/deftware/client/framework/item/items/BlockItem.java
@@ -1,10 +1,28 @@
 package me.deftware.client.framework.item.items;
 
 import me.deftware.client.framework.item.Item;
+import me.deftware.client.framework.item.ItemStack;
 import me.deftware.client.framework.world.block.Block;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+
+import java.util.function.Consumer;
+
 
 public interface BlockItem extends Item {
 
     Block getBlock();
+
+    static void getItems(ItemStack stack, Consumer<ItemStack> consumer) {
+        NBTTagCompound entityTag = ((net.minecraft.item.ItemStack) stack).getChildTag("BlockEntityTag");
+        if (entityTag != null && entityTag.contains("Items", 9)) {
+            NBTTagList items = entityTag.getList("Items", 10);
+            for (int i = 0; i < items.size(); i++) {
+                NBTTagCompound data = items.getCompound(i);
+                ItemStack itemStack = (ItemStack) net.minecraft.item.ItemStack.read(data);
+                consumer.accept(itemStack);
+            }
+        }
+    }
 
 }
--- src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
@@ -1,9 +1,15 @@
 package me.deftware.client.framework.network.packets;
 
 import me.deftware.client.framework.item.ItemStack;
+import me.deftware.client.framework.item.ItemTypes;
 import me.deftware.client.framework.network.PacketWrapper;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.network.Packet;
 import net.minecraft.util.EnumHand;
+
+import java.util.List;
 
 /**
  * @author Deftware
@@ -16,6 +22,18 @@ public class CPacketEditBook extends PacketWrapper {
 
     public CPacketEditBook(ItemStack book) {
         super(new net.minecraft.network.play.client.CPacketEditBook((net.minecraft.item.ItemStack) book, true, EnumHand.MAIN_HAND));
+    }
+
+    public static void setContents(ItemStack stack, List<String> pages) {
+        if (!ItemTypes.WritableBook.is(stack.getItem())) {
+            throw new IllegalArgumentException("The stack must be a writable book!");
+        }
+        NBTTagCompound nbt = ((net.minecraft.item.ItemStack) stack).getOrCreateTag();
+        NBTTagList list = new NBTTagList();
+        for (String page : pages) {
+            list.add(new NBTTagString(page));
+        }
+        nbt.put("pages", list);
     }
 
 }
