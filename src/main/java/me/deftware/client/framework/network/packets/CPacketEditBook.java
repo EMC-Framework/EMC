51d3578c3eb72081612f9bef6bb03d2d7afd9895
deftware <deftware@aristois.net>
2024-03-02 20:17:05 +0100

    Add nbt helper functions

--- src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
@@ -1,9 +1,12 @@
 package me.deftware.client.framework.network.packets;
 
 import me.deftware.client.framework.item.ItemStack;
+import me.deftware.client.framework.item.ItemTypes;
 import me.deftware.client.framework.network.PacketWrapper;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.network.Packet;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.network.packet.c2s.play.BookUpdateC2SPacket;
 
 import java.util.ArrayList;
@@ -37,6 +40,18 @@ public class CPacketEditBook extends PacketWrapper {
 
     public CPacketEditBook(ItemStack book) {
         super(of(book));
+    }
+
+    public static void setContents(ItemStack stack, List<String> pages) {
+        if (!ItemTypes.WritableBook.is(stack.getItem())) {
+            throw new IllegalArgumentException("The stack must be a writable book!");
+        }
+        NbtCompound nbt = ((net.minecraft.item.ItemStack) stack).getOrCreateTag();
+        NbtList list = new NbtList();
+        for (String page : pages) {
+            list.add(NbtString.of(page));
+        }
+        nbt.put("pages", list);
     }
 
 }
