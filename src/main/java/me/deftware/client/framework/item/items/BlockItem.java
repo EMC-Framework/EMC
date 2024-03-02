6999713ea491979bd2d6890ef1f84207b8edffe1
deftware <deftware@aristois.net>
2024-03-02 20:30:51 +0100

    Add nbt helper functions

--- src/main/java/me/deftware/client/framework/item/items/BlockItem.java
+++ src/main/java/me/deftware/client/framework/item/items/BlockItem.java
@@ -1,10 +1,28 @@
 package me.deftware.client.framework.item.items;
 
 import me.deftware.client.framework.item.Item;
+import me.deftware.client.framework.item.ItemStack;
 import me.deftware.client.framework.world.block.Block;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+
+import java.util.function.Consumer;
+
 
 public interface BlockItem extends Item {
 
     Block getBlock();
+
+    static void getItems(ItemStack stack, Consumer<ItemStack> consumer) {
+        CompoundTag entityTag = ((net.minecraft.item.ItemStack) stack).getSubTag("BlockEntityTag");
+        if (entityTag != null && entityTag.contains("Items", 9)) {
+            ListTag items = entityTag.getList("Items", 10);
+            for (int i = 0; i < items.size(); i++) {
+                CompoundTag data = items.getCompound(i);
+                ItemStack itemStack = (ItemStack) net.minecraft.item.ItemStack.fromTag(data);
+                consumer.accept(itemStack);
+            }
+        }
+    }
 
 }
