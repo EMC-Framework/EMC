4e751076a67d45694452644921955cd26b808870
deftware <deftware@aristois.net>
2024-03-02 20:45:56 +0100

    Add nbt helper functions

--- src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
+++ src/main/java/me/deftware/client/framework/network/packets/CPacketEditBook.java
@@ -2,11 +2,17 @@ package me.deftware.client.framework.network.packets;
 
 import io.netty.buffer.Unpooled;
 import me.deftware.client.framework.item.ItemStack;
+import me.deftware.client.framework.item.ItemTypes;
 import me.deftware.client.framework.network.PacketWrapper;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.network.Packet;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.network.play.client.CPacketCustomPayload;
 import net.minecraft.util.EnumHand;
+
+import java.util.List;
 
 /**
  * @author Deftware
@@ -22,6 +28,18 @@ public class CPacketEditBook extends PacketWrapper {
         PacketBuffer packetbuffer = new PacketBuffer(Unpooled.buffer());
         packetbuffer.writeItemStack((net.minecraft.item.ItemStack) book);
         packet = new CPacketCustomPayload("MC|BEdit", packetbuffer);
+    }
+
+    public static void setContents(ItemStack stack, List<String> pages) {
+        if (!ItemTypes.WritableBook.is(stack.getItem())) {
+            throw new IllegalArgumentException("The stack must be a writable book!");
+        }
+        NBTTagCompound nbt = ((net.minecraft.item.ItemStack) stack).getOrCreateTag();
+        NBTTagList list = new NBTTagList();
+        for (String page : pages) {
+            list.add(new NBTTagString(page));
+        }
+        nbt.put("pages", list);
     }
 
 }
