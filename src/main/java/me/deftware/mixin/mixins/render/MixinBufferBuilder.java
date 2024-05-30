f09f01317dc50aa921687481dc06343511bcad5f
deftware <deftware@aristois.net>
2024-05-30 23:29:13 +0200

    Fix MixinBufferBuilder alpha modifier

--- src/main/java/me/deftware/mixin/mixins/render/MixinBufferBuilder.java
+++ src/main/java/me/deftware/mixin/mixins/render/MixinBufferBuilder.java
@@ -3,23 +3,19 @@ package me.deftware.mixin.mixins.render;
 import me.deftware.client.framework.main.bootstrap.Bootstrap;
 import net.minecraft.client.render.BufferBuilder;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(BufferBuilder.class)
 public class MixinBufferBuilder {
 
-    /* TODO @ModifyVariable(method = "vertex(FFFFFFFFFIIFFF)V", at = @At("HEAD"), index = 7)
-    private float alpha(float alpha) {
-        if (Bootstrap.blockProperties.isActive() && Bootstrap.blockProperties.isOpacityMode())
-            return getCustomAlpha();
-        return alpha;
-    }*/
-
-    @Unique
-    private float getCustomAlpha() {
-        return Bootstrap.blockProperties.getOpacity() / 255f;
+    @ModifyVariable(method = "vertex(FFFIFFIIFFF)V", at = @At("HEAD"), index = 4, argsOnly = true)
+    private int alphaModifier(int color) { // argb
+        var props = Bootstrap.blockProperties;
+        if (props.isActive() && props.isOpacityMode()) {
+            int alpha = (int) props.getOpacity(); // 0 - 255
+            return (color & 0x00ffffff) | (alpha << 24);
+        }
+        return color;
     }
 
 }
-
