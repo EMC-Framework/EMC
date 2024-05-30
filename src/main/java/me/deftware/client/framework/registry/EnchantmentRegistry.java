63fa43f269c7430ba82b241780bff1ab64a5eed9
deftware <deftware@aristois.net>
2024-05-30 23:19:14 +0200

    Implement Enchantment->get(Protection|Damage)

--- src/main/java/me/deftware/client/framework/registry/EnchantmentRegistry.java
+++ src/main/java/me/deftware/client/framework/registry/EnchantmentRegistry.java
@@ -3,6 +3,10 @@ package me.deftware.client.framework.registry;
 import me.deftware.client.framework.item.Enchantment;
 
 import me.deftware.client.framework.message.Message;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.EnchantmentEffectComponentTypes;
+import net.minecraft.enchantment.effect.EnchantmentEffectEntry;
+import net.minecraft.enchantment.effect.EnchantmentValueEffect;
 import net.minecraft.registry.BuiltinRegistries;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
@@ -10,6 +14,7 @@ import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.entry.RegistryEntry;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -55,26 +60,33 @@ public enum EnchantmentRegistry implements IRegistry.IdentifiableRegistry<Enchan
 			return entry;
 		}
 
-		// TODO: Implement
-
 		@Override
 		public int getMinLevel() {
-			return 0;
+			return enchantment.getMinLevel();
 		}
 
 		@Override
 		public int getMaxLevel() {
-			return 0;
+			return enchantment.getMaxLevel();
+		}
+
+		private float sumValue(int level, ComponentType<List<EnchantmentEffectEntry<EnchantmentValueEffect>>> type) {
+			float sum = 0;
+			var list = enchantment.getEffect(type);
+			for (var value : list) {
+				sum += value.effect().apply(level, null, 0);
+			}
+			return sum;
 		}
 
 		@Override
 		public int getProtection(int level) {
-			return 0;
+			return (int) sumValue(level, EnchantmentEffectComponentTypes.DAMAGE_PROTECTION);
 		}
 
 		@Override
 		public float getDamage(int level) {
-			return 0;
+			return sumValue(level, EnchantmentEffectComponentTypes.DAMAGE);
 		}
 
 		@Override
