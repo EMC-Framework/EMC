c12e09753bdb178f03e072a3aa0d0796fe17f6de
deftware <deftware@aristois.net>
2024-06-24 19:14:19 +0200

    Update script

--- mcpconfig.sh
+++ mcpconfig.sh
@@ -2,7 +2,7 @@
 git clone -b jitpack https://github.com/DimensionalDevelopment/MCPConfig.git
 cd MCPConfig
 chmod +x gradlew
-./gradlew build
+./gradlew build -Dorg.gradle.java.home=~/.jdks/corretto-1.8.0_412
 cd build/distributions
 mkdir -p maven/com/github/DimensionalDevelopment/MCPConfig/jitpack-SNAPSHOT/
 cp mcp_config-1.13.2.zip maven/com/github/DimensionalDevelopment/MCPConfig/jitpack-SNAPSHOT/MCPConfig-jitpack-SNAPSHOT.zip
